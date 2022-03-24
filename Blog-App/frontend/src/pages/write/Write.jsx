a2f54e8076941576ff55f02fec1c25f138fdc99e
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-03-24 15:27:13 +0600

    write page

--- Blog-App/frontend/src/pages/write/Write.jsx
+++ Blog-App/frontend/src/pages/write/Write.jsx
@@ -1,5 +1,35 @@
 import "./write.css";
 
 export default function Write() {
-  return <div className="write">Write</div>;
+  return (
+    <div className="write">
+      <img
+        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+        className="writeImg"
+      />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fa-solid fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            placeholder="Input your title"
+            className="writeInput"
+            autoFocus
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            type="text"
+            placeholder="Tell your story"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  );
 }
