bdec88be1b42e5a50c76d45d81ae999cf7cb630e
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-03-24 16:51:35 +0600

    Register Page

--- Blog-App/frontend/src/App.js
+++ Blog-App/frontend/src/App.js
@@ -3,6 +3,7 @@ import SinglePost from "./components/singlePost/SinglePost";
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <TopBar />
-      <Login />
+      <Register />
     </>
   );
 }
