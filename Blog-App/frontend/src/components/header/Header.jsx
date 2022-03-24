f64a3d2076d7f4980c0158a02041121341c047af
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-03-24 10:53:46 +0600

    Finishing Top bar

---
+++ Blog-App/frontend/src/components/header/Header.jsx
@@ -0,0 +1,4 @@
+import "./header.css";
+export default function Header() {
+  return <div className="header">Header</div>;
+}
---
+++ Blog-App/frontend/src/components/topbar/TopBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./topbar.css";
+
+export default function TopBar() {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-facebook-square"></i>
+        <i className="topIcon fa-brands fa-twitter-square"></i>
+        <i className="topIcon fa-brands fa-pinterest-square"></i>
+        <i className="topIcon fa-brands fa-instagram-square"></i>
+      </div>
+
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACTS</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+
+      <div className="topRight">
+        <img
+          className="topImg"
+          src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          alt="profile"
+        />
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
+}
