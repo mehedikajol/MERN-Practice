e772e2aefe9ea1798e6adbf23a5932b02db7b361
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-03-24 17:28:07 +0600

    adding react router dom v6

--- Blog-App/frontend/src/App.js
+++ Blog-App/frontend/src/App.js
@@ -8,12 +8,22 @@ import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
+  const user = false;
   return (
-    <>
+    <BrowserRouter>
       <TopBar />
-      <Register />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
