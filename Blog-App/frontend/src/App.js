7aa526989621ff3d3bb85fc4c280fc924f41a74f
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-03-24 13:37:06 +0600

    creating write page

--- Blog-App/frontend/src/App.js
+++ Blog-App/frontend/src/App.js
@@ -1,12 +1,15 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import SinglePost from "./components/singlePost/SinglePost";
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Single />
+      <Write />
     </>
   );
 }
