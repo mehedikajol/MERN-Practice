c0c3a11dfcd3acb8cf5b196a5bc174dab7fa9e09
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-03-24 16:38:27 +0600

    login page

--- Blog-App/frontend/src/App.js
+++ Blog-App/frontend/src/App.js
@@ -2,6 +2,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import SinglePost from "./components/singlePost/SinglePost";
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <TopBar />
-      <Settings />
+      <Login />
     </>
   );
 }
