86c20f2298decf6db1a2e451530c10f6a7a9cfd6
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-03-24 23:40:20 +0600

    creating auth to create user

--- Blog-App/backend/index.js
+++ Blog-App/backend/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -12,6 +14,8 @@ mongoose
   })
   .then(console.log("connected to databse"))
   .catch((err) => console.log(err));
+
+app.use("/backend/auth", authRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running in port 5000");
