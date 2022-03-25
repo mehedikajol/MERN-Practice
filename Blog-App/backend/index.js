a37d4f92c35757ea16be0da14451331f8e8a48ec
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-03-25 09:31:24 +0600

    categories routes

--- Blog-App/backend/index.js
+++ Blog-App/backend/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const postsRoute = require("./routes/posts");
+const categoriesRoutes = require("./routes/categories");
 
 dotenv.config();
 app.use(express.json());
@@ -20,6 +21,7 @@ mongoose
 app.use("/backend/auth", authRoute);
 app.use("/backend/users", usersRoute);
 app.use("/backend/posts", postsRoute);
+app.use("/backend/categories", categoriesRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Backend is running in port ${process.env.PORT}`);
