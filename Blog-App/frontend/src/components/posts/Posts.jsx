5a8b7111649779b494ef04dc6469ee6132f0359d
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-03-25 15:35:01 +0600

    fetching category

--- Blog-App/frontend/src/components/posts/Posts.jsx
+++ Blog-App/frontend/src/components/posts/Posts.jsx
@@ -5,7 +5,7 @@ export default function Posts({ posts }) {
   return (
     <div className="posts">
       {posts.map((post) => (
-        <Post key={post.id} post={post} />
+        <Post key={post._id} post={post} />
       ))}
     </div>
   );
